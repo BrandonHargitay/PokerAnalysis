58fe790064dafd51687e2fdbd4ea65aa562e072b
Brandon <branhargy@gmail.com>
2022-11-06 20:58:13 -0800

    Add methods to dealCard and getCardAt

--- Deck.cpp
+++ Deck.cpp
@@ -5,14 +5,14 @@
 #include "Deck.h"
 
 Card Deck::dealCard() {
-    return Card();
+    return deck[cardIndex++];
 }
 
 Card Deck::getCardAt(int index) {
-    return Card();
+    return deck[index];
 }
 
-Deck::Deck() {
+Deck::Deck() : cardIndex(0){
     init();
 }
 
@@ -31,69 +31,12 @@ bool Deck::empty() {
 }
 
 void Deck::fillDeck() {
-    SuitENum suit = SPADES;
-    RankENum rank = ACE;
+
     int index = 0;
     for (int i = 0; i < SUIT_MAX; i++) {
         for (int j = 0; j < RANK_MAX; j++) {
+            //assigns card to deck by casting i into a SuiteEnum and j into a RankEnum
             deck[index] = Card(static_cast<SuitENum>(i), static_cast<RankENum>(j+1));
-
-//            switch (i) {
-//                case 0:
-//                    suit = HEARTS;
-//                    break;
-//                case 1:
-//                    suit = DIAMONDS;
-//                    break;
-//                case 2:
-//                    suit = SPADES;
-//                    break;
-//                case 3:
-//                    suit = CLUBS;
-//                    break;
-//            }
-//            switch (j) {
-//                case 0:
-//                    rank = ACE;
-//                    break;
-//                case 1:
-//                    rank = TWO;
-//                    break;
-//                case 2:
-//                    rank = THREE;
-//                    break;
-//                case 3:
-//                    rank = FOUR;
-//                    break;
-//                case 4:
-//                    rank = FIVE;
-//                    break;
-//                case 5:
-//                    rank = SIX;
-//                    break;
-//                case 6:
-//                    rank = SEVEN;
-//                    break;
-//                case 7:
-//                    rank = EIGHT;
-//                    break;
-//                case 8:
-//                    rank = NINE;
-//                    break;
-//                case 9:
-//                    rank = TEN;
-//                    break;
-//                case 10:
-//                    rank = JACK;
-//                    break;
-//                case 11:
-//                    rank = QUEEN;
-//                    break;
-//                case 12:
-//                    rank = KING;
-//                    break;
-//            }
-//            deck[index] = Card(suit, rank);
             index++;
         }
 
@@ -102,7 +45,7 @@ void Deck::fillDeck() {
 
 void Deck::init() {
     fillDeck();
-    //shuffle();
+    shuffle();
 }
 
 std::ostream& operator<<(std::ostream& out, const Deck& deck){
@@ -112,3 +55,5 @@ std::ostream& operator<<(std::ostream& out, const Deck& deck){
     }
     return out;
 }
+
+
