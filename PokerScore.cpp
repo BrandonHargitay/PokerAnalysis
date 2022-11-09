5d2af4882a5d5b199cbb9bd150ad5ebcd9d5ab6b
Brandon <branhargy@gmail.com>
2022-11-08 20:34:40 -0800

    Add isOnePair() & isHighCard

--- PokerScore.cpp
+++ PokerScore.cpp
@@ -3,3 +3,53 @@
 //
 
 #include "PokerScore.h"
+
+
+bool PokerScore::isRoyalFlush(CardHand hand) {
+    return false;
+}
+
+bool PokerScore::isStraightFLush(CardHand hand) {
+    return false;
+}
+
+bool PokerScore::isFourOfKind(CardHand hand) {
+    return false;
+}
+
+bool PokerScore::isFullHouse(CardHand hand) {
+    return false;
+}
+
+bool PokerScore::isFlush(CardHand hand) {
+    return false;
+}
+
+bool PokerScore::isStraight(CardHand hand) {
+    return false;
+}
+
+bool PokerScore::isThreeofKind(CardHand hand) {
+    return false;
+}
+
+bool PokerScore::IsTwoPair(CardHand hand) {
+    return false;
+}
+
+bool PokerScore::isOnePair(CardHand hand) {
+
+    for(int i = 0; i < hand.getSize(); i++){
+        if(hand.getNextCard(i).getRank() == hand.getNextCard(i+1).getRank()){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool PokerScore::isHighCard(CardHand hand) {
+    if(hand.getSize() >0 ){
+        return true;
+    }
+    return false;
+}
--- PokerScore.h
+++ PokerScore.h
@@ -4,10 +4,53 @@
 
 #ifndef POKER_POKERSCORE_H
 #define POKER_POKERSCORE_H
-
+#include "CardHand.h"
+#include <vector>
 
 class PokerScore {
+public:
+    enum Scores{
 
+        ROYAL_FLUSH, //A, K, Q, J, 10, all the same suit.
+
+        STRAIGHT_FLUSH, //five cards of the same suit and consecutive ranking
+
+        FOUR_OF_A_KIND, //four cards of the same ranking
+
+        FULL_HOUSE, //three cards of the same rank along with two cards of the same rank
+
+        FLUSH, //five cards of the same suit
+
+        STRAIGHT, //five cards in consecutive ranking
+
+        THREE_OK_A_KIND, //three cards of the same rank
+
+        TWO_PAIR, //two cards of the same rank along with another two cards of the same rank
+
+        ONE_PAIR, //two cards of the same rank
+
+        HIGH_CARD //highest card in the player’s hand
+
+    };
+
+    static bool isRoyalFlush(CardHand hand);
+    static bool isStraightFLush(CardHand hand);
+    static bool isFourOfKind(CardHand hand);
+    static bool isFullHouse(CardHand hand);
+    static bool isFlush(CardHand hand);
+    static bool isStraight(CardHand hand);
+    static bool isThreeofKind(CardHand hand);
+    static bool IsTwoPair(CardHand hand);
+    static bool isOnePair(CardHand hand);
+    static bool isHighCard(CardHand hand);
+
+
+
+    void operator+=(const Scores& score);
+    friend bool operator==(const PokerScore& p, Scores score);
+    PokerScore();
+private:
+    std::vector<Scores> scores;
 };
 
 
