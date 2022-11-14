d32a161264ea728eeaac54af3cf192cb54fa481e
Brandon <branhargy@gmail.com>
2022-11-14 12:49:38 -0800

    Add setString for statistics

--- Drawables/DrawStatistics.cpp
+++ Drawables/DrawStatistics.cpp
@@ -5,32 +5,26 @@
 #include "DrawStatistics.h"
 #include "../PokerScore.h"
 #include <iostream>
-#include <array>
 
-DrawStatistics::DrawStatistics() {
+
+
+DrawStatistics::DrawStatistics(double n) : numberOfHands(n)  {
     init();
 }
 
 void DrawStatistics::displayStatistics() {
-    std::array<int, 10> scores = PokerScore::getScores();
-    std::array<std::string,10> scoresToString;
-
-    for(int i = 0; i < 10; i++){
-        scoresToString[i] = std::to_string(scores[i]);
-    }
-
-
+    convertScoresToStrings();
     statistics.setFont(font);
     //this is ugly but I could not think of a different way to format the display
-    statistics.setString("High Card: " + scoresToString[0] + "          One Pair: " + scoresToString[1] +
-                        "           Two Pair: " + scoresToString[2] + "      Three of a kind: " + scoresToString[3] +
-                        "\n Straight: "+ scoresToString[4] + "                 Flush: " + scoresToString[5] +
-                        "                     Full House: " + scoresToString[6] + "        Four of a Kind: " + scoresToString[7] +
-                        "\n                                              Straight: " + scoresToString[8] + "                              Royal Flush: " + scoresToString[9]);
+    statistics.setString("High Card: " + scoresToString[0]  + "%          One Pair: " + scoresToString[1] +
+                        "%           Two Pair: " + scoresToString[2] + "%      Three of a kind: " + scoresToString[3] +
+                        "%\nStraight: "+ scoresToString[4] + "%            Flush: " + scoresToString[5] +
+                        "%           Full House: " + scoresToString[6] + "%      Four of a Kind: " + scoresToString[7] +
+                        "%\n                                   Straight: " + scoresToString[8] + "%                              Royal Flush: " + scoresToString[9]+"%");
     statistics.setCharacterSize(40);
     statistics.setLineSpacing(3.5);
     statistics.setFillColor(sf::Color::Black);
-    statistics.setPosition(120,500);
+    statistics.setPosition(50,500);
 }
 void DrawStatistics::draw(sf::RenderTarget& window, sf::RenderStates states) const{
     window.draw(title);
@@ -50,3 +44,15 @@ void DrawStatistics::displayTitle() {
     title.setCharacterSize(70);
     title.setFillColor(sf::Color::Black);
 }
+
+void DrawStatistics::convertScoresToStrings() {
+    for(int i = 0; i < 10; i++){
+        double percent =((double)PokerScore::getScores()[i]/ numberOfHands) * 100.;
+        std::string str = std::to_string(percent);
+        str.erase ( str.find_last_not_of('0') + 1, std::string::npos );
+        str.erase ( str.find_last_not_of('.') + 1, std::string::npos );
+        scoresToString[i] = str;
+    }
+}
+
+
--- Drawables/DrawStatistics.h
+++ Drawables/DrawStatistics.h
@@ -5,16 +5,20 @@
 #ifndef POKER_DRAWSTATISTICS_H
 #define POKER_DRAWSTATISTICS_H
 #include <SFML/Graphics.hpp>
+#include <array>
 
 class DrawStatistics : public sf::Text{
 public:
     void displayStatistics();
-    DrawStatistics();
+    DrawStatistics(double n);
     virtual void draw(sf::RenderTarget& window, sf::RenderStates states) const;
 private:
     sf::Font font;
     sf::Text title;
     sf::Text statistics;
+    double numberOfHands;
+    std::array<std::string,10> scoresToString;
+    void convertScoresToStrings();
     void init();
     void displayTitle();
 
